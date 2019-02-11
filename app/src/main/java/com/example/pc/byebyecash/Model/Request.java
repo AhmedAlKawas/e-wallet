75bce2934ce93c79f5424127b9e2ef1c842e7462
AhmedAlKawas <46962787+AhmedAlKawas@users.noreply.github.com>
2019-02-11 22:57:01 +0200

    add sender mobile and role and status to the request model

--- app/src/main/java/com/example/pc/byebyecash/Model/Request.java
+++ app/src/main/java/com/example/pc/byebyecash/Model/Request.java
@@ -4,13 +4,43 @@ public class Request {
 
     private float credit;
     private String response;
+    private String senderMobile;
+    private String senderRole;
+    private String status;
 
-    public Request(float credit, String response) {
+    public Request(float credit, String response, String senderMobile, String senderRole, String status) {
         this.credit = credit;
         this.response = response;
+        this.senderMobile = senderMobile;
+        this.senderRole = senderRole;
+        this.status = status;
     }
 
     public Request() {
+    }
+
+    public String getSenderMobile() {
+        return senderMobile;
+    }
+
+    public void setSenderMobile(String senderMobile) {
+        this.senderMobile = senderMobile;
+    }
+
+    public String getSenderRole() {
+        return senderRole;
+    }
+
+    public void setSenderRole(String senderRole) {
+        this.senderRole = senderRole;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public float getCredit() {
